567c5fdbbc42cb8693c6fd86c36bb806f23b1bf2
Kilter <mateodaza@gmail.com>
2023-05-29 17:22:04 -0500

    adds rewards to account type

--- src/givfi-beefy-v6.ts
+++ src/givfi-beefy-v6.ts
@@ -10,27 +10,24 @@ import {
 import {
   Account,
   Balance,
-  Approval,
   FeeRecipientSet,
   FeeSet,
   FeesPaid,
-  Initialized,
   Transfer,
 } from '../generated/schema';
 
 export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.owner = event.params.owner;
-  entity.spender = event.params.spender;
-  entity.value = event.params.value;
-
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
-
-  entity.save();
+  // NOT IMPORTANT
+  // let entity = new Approval(
+  //   event.transaction.hash.concatI32(event.logIndex.toI32())
+  // );
+  // entity.owner = event.params.owner;
+  // entity.spender = event.params.spender;
+  // entity.value = event.params.value;
+  // entity.blockNumber = event.block.number;
+  // entity.blockTimestamp = event.block.timestamp;
+  // entity.transactionHash = event.transaction.hash;
+  // entity.save();
 }
 
 export function handleFeeRecipientSet(event: FeeRecipientSetEvent): void {
@@ -73,50 +70,85 @@ export function handleFeesPaid(event: FeesPaidEvent): void {
   entity.transactionHash = event.transaction.hash;
 
   entity.save();
+
+  let account = Account.load(event.params.user.toHex());
+  if (account != null) {
+    // Update rewards for the account
+    let rewards = account.rewards || BigInt.fromI32(0);
+    account.rewards = rewards.plus(event.params.amount);
+    account.save();
+  }
 }
 
 export function handleInitialized(event: InitializedEvent): void {
-  let entity = new Initialized(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.version = event.params.version;
-
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
-
-  entity.save();
+  // NOT IMPORTANT
+  // let entity = new Initialized(
+  //   event.transaction.hash.concatI32(event.logIndex.toI32())
+  // );
+  // entity.version = event.params.version;
+  // entity.blockNumber = event.block.number;
+  // entity.blockTimestamp = event.block.timestamp;
+  // entity.transactionHash = event.transaction.hash;
+  // entity.save();
 }
 
 export function handleTransfer(event: TransferEvent): void {
-  let accountId = event.params.to.toHex();
-  let account = Account.load(accountId);
-  if (account == null) {
-    account = new Account(accountId);
-    account.save();
+  let recipientAddressHex = event.params.to.toHex();
+  let vault = Account.load(recipientAddressHex);
+  if (vault == null) {
+    vault = new Account(recipientAddressHex);
+    vault.rewards = BigInt.fromI32(0);
+    vault.save();
   }
 
-  let entity = new Transfer(
+  let senderAddressHex = event.params.from.toHex();
+  let staker = Account.load(senderAddressHex);
+  if (
+    staker == null &&
+    senderAddressHex != '0x0000000000000000000000000000000000000000'
+  ) {
+    staker = new Account(senderAddressHex);
+    staker.rewards = BigInt.fromI32(0);
+    staker.save();
+  }
+
+  let transferRecord = new Transfer(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   );
-  entity.from = event.params.from;
-  entity.to = event.params.to;
-  entity.value = event.params.value;
+  transferRecord.from = event.params.from;
+  transferRecord.to = event.params.to;
+  transferRecord.value = event.params.value;
+  transferRecord.blockNumber = event.block.number;
+  transferRecord.blockTimestamp = event.block.timestamp;
+  transferRecord.transactionHash = event.transaction.hash;
+  transferRecord.save();
 
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
-
-  entity.save();
-
-  let balanceId = 'GIVFiVault-' + account.id;
-  let balance = Balance.load(balanceId);
-  if (balance == null) {
-    balance = new Balance(balanceId);
-    balance.account = account.id;
-    balance.value = BigInt.fromI32(0);
+  let vaultBalanceID = recipientAddressHex;
+  let vaultBalance = Balance.load(vaultBalanceID);
+  if (vaultBalance == null) {
+    vaultBalance = new Balance(vaultBalanceID);
+    vaultBalance.account = vault.id;
+    vaultBalance.value = BigInt.fromI32(0);
+    vaultBalance.timestamp = event.block.timestamp;
   }
-  balance.value = balance.value.plus(event.params.value);
-  balance.timestamp = event.block.timestamp;
-  balance.save();
+
+  vaultBalance.value = vaultBalance.value.plus(event.params.value);
+  vaultBalance.save();
+
+  if (
+    senderAddressHex != '0x0000000000000000000000000000000000000000' &&
+    staker != null
+  ) {
+    let stakerBalanceID = senderAddressHex;
+    let stakerBalance = Balance.load(stakerBalanceID);
+    if (stakerBalance == null) {
+      stakerBalance = new Balance(stakerBalanceID);
+      stakerBalance.account = staker.id;
+      stakerBalance.value = BigInt.fromI32(0);
+      stakerBalance.timestamp = event.block.timestamp;
+    }
+
+    stakerBalance.value = stakerBalance.value.minus(event.params.value);
+    stakerBalance.save();
+  }
 }
